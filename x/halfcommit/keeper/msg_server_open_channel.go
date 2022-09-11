68022ef6b8106dc7e773fe140e3e9f3be5fe535c
LAP02459 <dung.tran8@tiki.vn>
2022-09-11 11:44:18 +0700

    complete open-channel & close-channel msg

--- x/halfcommit/keeper/msg_server_open_channel.go
+++ x/halfcommit/keeper/msg_server_open_channel.go
@@ -2,33 +2,34 @@ package keeper
 
 import (
 	"context"
-	"fmt"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"halfcommit/x/halfcommit/types"
 )
 
 func (k msgServer) OpenChannel(goCtx context.Context, msg *types.MsgOpenChannel) (*types.MsgOpenChannelResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
+
 	addreesB, err := sdk.AccAddressFromBech32(msg.PartB)
 	if err != nil {
 		return nil, err
 	}
-	accB := k.ak.GetAccount(ctx, addreesB)
-	if accB == nil {
-		return nil, fmt.Errorf("test B")
-	}
+
+	//accB := k.ak.GetAccount(ctx, addreesB)
+	//if accB == nil {
+	//	return nil, fmt.Errorf("test B")
+	//}
 
 	addreesA, err := sdk.AccAddressFromBech32(msg.PartA)
 	if err != nil {
 		return nil, err
 	}
-	accA := k.ak.GetAccount(ctx, addreesA)
-	if accA == nil {
-		return nil, fmt.Errorf("test A")
-	}
+	//accA := k.ak.GetAccount(ctx, addreesA)
+	//if accA == nil {
+	//	return nil, fmt.Errorf("test A")
+	//}
 
 	multiAddr := msg.GetSigners()[0]
+
 	k.bankKeeper.SendCoins(ctx, addreesA, multiAddr, sdk.Coins{*msg.CoinA})
 	k.bankKeeper.SendCoins(ctx, addreesB, multiAddr, sdk.Coins{*msg.CoinB})
 
